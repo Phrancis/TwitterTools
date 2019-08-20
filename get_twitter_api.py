3940cd15b34088a3e9f934e9ff6a4693a4b06ba7
Phrancis <Phrancis@users.noreply.github.com>
2019-08-20 13:32:26 -0400

    Create API test to confirm authentication works when running get_twitter_api.py ad hoc.

--- get_twitter_api.py
+++ get_twitter_api.py
@@ -11,21 +11,27 @@ def get_twitter_api(file_path: str = 'app_data/twitter_api_keys.json') -> API:
     """
     try:
         _file: TextIO
+        _keys: Dict = dict()
         with open(file_path, 'r') as _file:
-            keys: Dict = json.load(_file)
+            _keys = json.load(_file)
     except FileNotFoundError as _err:
-        print(f'File not found: {file_path}')
-        print('Run "init_twitter_api.py" first, or verify the file path.')
+        print(f'File not found: {file_path}\nRun "init_twitter_api.py" first, or verify the file path.')
         raise _err
 
-    auth: OAuthHandler = OAuthHandler(keys['api_key'], keys['api_secret'])
-    auth.set_access_token(keys['access_token'], keys['access_secret'])
+    # Make sure keys are not empty
+    if not bool(_keys):
+        raise ValueError(f'No data found in {file_path}\nRun "init_twitter_api.py" first, or verify the file path.')
+
+    _auth: OAuthHandler = OAuthHandler(_keys['api_key'], _keys['api_secret'])
+    _auth.set_access_token(_keys['access_token'], _keys['access_secret'])
     return API(
-        auth,
+        _auth,
         wait_on_rate_limit=True,
         wait_on_rate_limit_notify=True,
         compression=True)
 
 
 if __name__ == '__main__':
-    print(get_twitter_api())
+    api: API = get_twitter_api()
+    # Query the API for self data to make sure it's working
+    print(f'Current API user: {api.me()._json["screen_name"]}')
