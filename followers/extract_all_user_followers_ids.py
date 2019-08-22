0a73dcd5e532a0494746486778f5f4e696657d1e
Phrancis <Phrancis@users.noreply.github.com>
2019-08-22 00:29:05 -0400

    Clean up a bit, because #NamingThings is hard

--- followers/extract_all_user_followers_ids.py
+++ followers/extract_all_user_followers_ids.py
@@ -1,11 +1,11 @@
 import datetime
 import os
-from typing import List, TextIO
+from typing import TextIO
 
 import tweepy
 from tweepy import TweepError
 
-from api import get_twitter_api as api
+from api import get_twitter_api as tweepy_api
 
 PATH_TO_APP_DATA = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'app_data')
 
@@ -16,12 +16,11 @@ def extract_all_user_followers_ids(screen_name: str) -> str:
     :param screen_name: The screen name of the account to query. If left blank, will use the API user's screen name.
     :return: The file path where the queried data was written.
     """
-    _api = api.get_twitter_api(os.path.join(PATH_TO_APP_DATA, 'twitter_api_keys.json'))
+    _api = tweepy_api.get_twitter_api(os.path.join(PATH_TO_APP_DATA, 'twitter_api_keys.json'))
     if screen_name == '' or not screen_name:
-        screen_name = api.get_current_user_json_field('screen_name')
+        screen_name = tweepy_api.get_current_user_json_field('screen_name')
     _output_file_name = f'user_followers_ids {screen_name.lower()} {datetime.date.today()}.txt'
     _output_file_path = os.path.join(PATH_TO_APP_DATA, _output_file_name)
-    _followers_count: int = 0
     try:
         _output_file: TextIO
         with open(_output_file_path, 'w') as _output_file:
@@ -44,10 +43,10 @@ if __name__ == '__main__':
     # Just in case they don't read instructions ;-)
     _screen_name = _screen_name.replace('@', '')
 
-    _api = api.get_twitter_api(os.path.join(PATH_TO_APP_DATA, 'twitter_api_keys.json'))
+    main_api = tweepy_api.get_twitter_api(os.path.join(PATH_TO_APP_DATA, 'twitter_api_keys.json'))
     if _screen_name == '' or not _screen_name:
-        _screen_name = _api.me()._json['screen_name']
-    _target_user = _api.get_user(screen_name=_screen_name)
+        _screen_name = main_api.me()._json['screen_name']
+    _target_user = main_api.get_user(screen_name=_screen_name)
     _target_followers_count: int = _target_user._json['followers_count']
     print(f'{datetime.datetime.now()} Target user\'s followers count: {_target_followers_count}')
     print(f'{datetime.datetime.now()} Guesstimated query time (factoring free API rate limits but not your recent usage, YMMV): {_target_followers_count / 5000} minutes.')
