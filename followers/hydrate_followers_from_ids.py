b3f431fd9492e5690670e6a52ba543c1e004b22f
Phrancis <Phrancis@users.noreply.github.com>
2019-08-23 04:50:11 -0400

    Tonight, we succeed querying users. Tomorrow, we dine in file I/O hell

--- followers/hydrate_followers_from_ids.py
+++ followers/hydrate_followers_from_ids.py
@@ -1,4 +1,5 @@
 import os
+import json
 from typing import TextIO, List
 
 import tweepy
@@ -12,18 +13,23 @@ api = tweepy_api.get_twitter_api(os.path.join(PATH_TO_APP_DATA, 'twitter_api_key
 
 def hydrate_followers_from_ids(input_file_path: str) -> None:
     # Read file and parse into a list of user IDs
-    followers_ids: TextIO
-    followers_ids_list: List
-    with open(input_file_path, 'r') as followers_ids:
-        followers_ids_list = followers_ids.read().splitlines()
-    # Iterate list to query API, 100 at a time
-    for user_id_index in range(0, len(followers_ids_list), 100):
-        current_list: List = followers_ids_list[user_id_index:user_id_index + 100]
-    # Following is for testing only
-        for item_index in range(0, len(current_list)):
-            print(f'{user_id_index + item_index} : {followers_ids_list[user_id_index + item_index]}')
-        print(f'Printed {len(current_list)} items')
-    print(f'All Done. Total {len(followers_ids_list)}')
+    followers_ids_input_file: TextIO
+    all_followers_ids_list: List
+    with open(input_file_path, 'r') as followers_ids_input_file:
+        all_followers_ids_list = followers_ids_input_file.read().splitlines()
+    all_followers_hydrated_list: List = []
+    # Iterate list of all followers, 100 at a time, because that's the max the Twitter API allows per request
+    for i_100_followers_slice in range(0, len(all_followers_ids_list), 100):
+        # Slice 100 IDs from followers
+        short_100_list: List = all_followers_ids_list[i_100_followers_slice:i_100_followers_slice + 100]
+        # Query Twitter API with the 100 IDs
+        hydrated_short_100_list: List = api.lookup_users(user_ids=short_100_list)
+        # Testing adding from short list
+        for user_entry in hydrated_short_100_list:
+            all_followers_hydrated_list.append(user_entry)
+    # Testing running through whole list
+    for i_item in range(0, len(all_followers_hydrated_list)):
+        print(f'{i_item}\t{all_followers_hydrated_list[i_item]._json}')
 
 
 if __name__ == '__main__':
